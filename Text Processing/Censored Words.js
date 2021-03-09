313dd71d43ce1f9c2377349ebf7e99f5f2914180
BorisLazarovv <77002959+BorisLazarovv@users.noreply.github.com>
2021-03-09 08:54:23 +0200

    Delete Text Processing directory

--- Text Processing/Censored Words.js
+++
@@ -1,4 +0,0 @@
-function solve(text, word) {
-    console.log(text.split(word).join('*'.repeat(word.length)));
-}
-solve('A small sentence with some words', 'small');
--- Text Processing/Lab/Cut and Reverse.js
+++
@@ -1,8 +0,0 @@
-function solve(str) {
-    let firstHalf = str.substring(0, str.length / 2);
-    let secondHalf = str.substring(str.length/2)
-    console.log(firstHalf.split('').reverse().join(''));
-    console.log(secondHalf.split('').reverse().join(''));
-
-}
-solve('tluciffiDsIsihTgnizamAoSsIsihT')
